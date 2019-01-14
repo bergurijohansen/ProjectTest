d01e1a8099f730c3ddbaf520ad36b931fbf17697
Bergur Johansen <bergurij@gmail.com>
2019-01-14 22:19:05 +0000

    sdf

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "=========================================================================" << std::endl;
+	std::cout << "= 1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18  19  20 =" << std::endl;
+	std::cout << "=========================================================================" << std::endl;
+
+	const size_t SIZE = 10;
+	int IntArray[SIZE];
+
+	int array1[10] = { 1,2,3,4,5,6,7,8,9,0 };
+
+
+
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		std::cout << array1[i] << std::endl;
+	}
+
+
+
+	getchar();
+
+	return 0;
+}
