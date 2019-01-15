9e3e7f81058f071deecd9d81fff4e15e3742c54d
Bergur Johansen <bergurij@gmail.com>
2019-01-15 21:08:19 +0000

    multi screens

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -10,13 +10,14 @@ int main()
 	const size_t SIZE = 10;
 	int IntArray[SIZE];
 
-	int array1[10] = { 1,2,3,4,5,6,7,8,9,0 };
+	char lcd1[10] = { '1','2','3','4','5','6','7','8','9','0' };
+	char lcd2[10] = { 'a','b','c','d','e','f','g','h','i','j' };
 
+	char* lcd[2] = { lcd1, lcd2 };
 
-
-	for (size_t i = 0; i < SIZE; i++)
+	for (size_t i = 0; i < SIZE*2; i++)
 	{
-		std::cout << array1[i] << std::endl;
+		std::cout << lcd[i/SIZE][i%SIZE] << std::endl;
 	}
 
 
